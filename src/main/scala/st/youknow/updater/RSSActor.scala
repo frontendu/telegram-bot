a58b6c0b5fe520414919114568168f4e5638b6c4
synnz <escape6661@gmail.com>
2018-10-18 03:30:37 +0700

    format output

--- src/main/scala/st/youknow/updater/RSSActor.scala
+++ src/main/scala/st/youknow/updater/RSSActor.scala
@@ -3,6 +3,7 @@ package st.youknow.updater
 import akka.NotUsed
 import akka.actor.{Actor, ActorRef, Props}
 import com.softwaremill.sttp.{HttpURLConnectionBackend, Id, SttpBackend, UriInterpolator, sttp}
+
 import scala.concurrent.ExecutionContextExecutor
 import st.youknow.updater.RSSActor.{LastPodcast, TGResponse}
 
@@ -10,8 +11,11 @@ import scala.concurrent.duration._
 
 object RSSActor {
   def apply(botActor: ActorRef): Props = Props(classOf[RSSActor], botActor)
+
   case class LastPodcast(title: String, pubDate: String, link: String, summary: String)
+
   case class TGResponse(text: String)
+
 }
 
 class RSSActor(botActor: ActorRef) extends Actor {
@@ -31,15 +35,35 @@ class RSSActor(botActor: ActorRef) extends Actor {
       val xml = (scala.xml.XML.loadString(rss.body.right.get) \ "channel" \ "item").head
       val entry = LastPodcast((xml \ "title").text, (xml \ "pubDate").text, (xml \ "link").text, (xml \ "summary").text)
 
+      val parsedSummary = parseSummary(entry.summary)
       val template =
         s"""
-           |*${entry.title}*
+           |*${parseTitle(entry.title)}*
            |
-           |${entry.summary}
+           |[Слушать подкаст](${entry.link})
            |
-           |[Слушать подкаст](${entry.link}
+           |${parsedSummary._1}
+           |
+           |${parsedSummary._2}
          """.stripMargin
 
       botActor ! TGResponse(template)
   }
+
+  def parseSummary(text: String): (String, String) = {
+    val filteredText = text.stripMargin.split('\n').filterNot(_.trim.isEmpty)
+    val summary = filteredText.filterNot(_.indexOf("http") > 0).mkString("\n")
+    val links = filteredText.filter(_.indexOf("http") > 0).map { x =>
+      val splitPos = x.indexOf("http")
+      if (splitPos > 0) {
+        val link = x.splitAt(splitPos - 1).productIterator.toList.map(_.toString.trim)
+        s"[${link.head}](${link.last})"
+      }
+      else x
+    }.mkString("\n")
+
+    (summary, links)
+  }
+
+  def parseTitle(text: String): String = text.replace("#", "")
 }
